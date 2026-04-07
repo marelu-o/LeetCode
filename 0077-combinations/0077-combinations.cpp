b992d2b7d719be66e60f681b0b2e87a820ff598a
Maria Fernanda Melo <mfsmelo06@gmail.com>
2026-04-07 17:41:50 -0300

    Time: 47 ms (54.19%), Space: 62.4 MB (95.88%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -8,15 +8,18 @@ public:
         for (int i = 1; i < n +1; i++){
            vetor.push_back(i);
         }
+        vector<int> combAtual(k);
+
         do {
-            vector<int> combAtual;
+            int indice = 0;
             for(int i = 0; i< n; i++){
                if (mascara[i]){
-               combAtual.push_back(vetor[i]);
+               combAtual[indice]= vetor[i];
+               indice ++;
                }
               
             }
-             resultado.push_back(combAtual);
+            resultado.push_back(combAtual);
         } while (next_permutation(mascara.begin(), mascara.end()));
          return resultado;
     }
