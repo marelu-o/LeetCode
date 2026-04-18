9beb7ee28d7c7d996e5adeed089760803ba2fc64
Maria Fernanda Melo <mfsmelo06@gmail.com>
2026-04-18 15:12:53 -0300

    Time: 0 ms (100%), Space: 17.5 MB (71.66%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        for ( int i = 2; i < cost.size(); i++){
+             cost[i] +=  min(cost[i-1], cost[i-2]);
+        }
+
+        return min(cost[cost.size() - 1], cost[cost.size() - 2]);
+        }
+    };
+
+    /* Didática do código:
+        #  cost = [ 1 ,100, 1 , 1, 1 ,100, 1 , 1 , 100, 1 ]
+        #  índices=[ 0,   1, 2 , 3, 4,   5, 6,  7,    8, 9 ]
+
+        # for ( int i = 2; i < cost.size(); i++) inicia no índice 2 e vai até o tamanho do vetor
+
+        # cost[2] = 1 + minímo entre (cost[1], cost[0]) = 1 + 1 = 2
+        # cost[3] = 1 + minímo entre (cost[2], cost[1]) = 1 + 2 = 3
+        # cost[4] = 1 + minímo entre (cost[3], cost[2]) = 1 + 2 = 3
+        # cost[5] = 100 + minímo entre (cost[4], cost[3]) = 1 + 3 = 104
+        # cost[6] = 1 + minímo entre (cost[5], cost[4]) = 1 + 3 = 4
+        # cost[7] = 1 + minímo entre (cost[6], cost[5]) = 1 + 4 = 5
+        # cost[8] = 100 + minímo entre (cost[7], cost[6]) = 100 + 4 = 104
+        # cost[9] = 1 + minímo entre (cost[8], cost[7]) = 1 + 5 = 6
+
+        #return min(cost[cost.size() - 1], cost[cost.size() - 2]) = return(min(cost[9], cost[8])) == 6
+
+        */
