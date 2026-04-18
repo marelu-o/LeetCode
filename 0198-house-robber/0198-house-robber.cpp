9a15c925305af3f0d2298a9973284614a0cdf3c5
Maria Fernanda Melo <mfsmelo06@gmail.com>
2026-04-18 15:48:50 -0300

    Time: 0 ms (100%), Space: 10.2 MB (75.81%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -12,3 +12,21 @@ public:
         return nums.back();
     }
 };
+
+ /* Didática do código:
+        #  nums = [ 2, 7, 9, 3, 1 ]
+        #  índices=[0, 1, 2, 3, 4 ]
+
+        #  if (nums.size() == 1) return nums[0]; -> Se o tamanho do vetor for igual à 1, retorna o único valor  presente no vetor.
+        
+        # nums[1] = max(nums[0], nums[1]); -> O índice 1 recebe o maior valor entre o índice 0 e o índice 1 (Ótimo para vetores de apenas 2 índices, 0 e 1);
+
+        # for (int i = 2; i < nums.size(); i++) -> Inicia no segundo índice e vai adiante
+
+        # nums[2] = máximo entre (nums[2] + nums [0], nums[1]) = (9 + 2, 7) = 11
+        # nums[3] = máximo entre (nums[3] + nums [1], nums[2]) = (3 + 7, 11) = 11
+        # nums[4] = máximo entre (nums[4] + nums [2], nums[3]) = (1 + 11, 11) = 12
+        
+        # return nums.back(); -> retorna o último valor presente na lista = nums[4] = 12
+
+        */
