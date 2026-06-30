e7b9837895759957ac824591e88e3736cf6b6861
Maria Fernanda Melo <mfsmelo06@gmail.com>
2026-06-30 13:55:41 -0300

    Time: 0 ms (100%), Space: 12.4 MB (56.17%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def climbStairs(self, n):
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+
+        if (n == 1) or (n == 0):
+            return dp[n]
+        else:
+            for i in range (2, n + 1):
+                dp[i] = dp[i-1] + dp[i-2]
+
+            return dp[n]
+        
