cb916c217ff66cbaa5b4fba5543f52bb0e8a079e
Maria Fernanda Melo <mfsmelo06@gmail.com>
2026-07-07 11:21:03 -0300

    Time: 18 ms (88.06%), Space: 13.2 MB (61.62%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -5,8 +5,7 @@ class Solution(object):
         for i in range (len(nums) - 1, - 1, -1):
             if i + nums[i] >= meta:
                 meta = i
-        if meta == 0:
-            return True
-        else:
-            return False
+        
+        return meta == 0
+            
         
