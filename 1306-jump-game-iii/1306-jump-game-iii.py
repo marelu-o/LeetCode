5c60d28732930d69771cd07182315c61ca09eaf2
Maria Fernanda Melo <mfsmelo06@gmail.com>
2026-07-14 11:14:31 -0300

    Time: 31 ms (46.36%), Space: 57.2 MB (42.93%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.py
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -3,14 +3,24 @@ class Solution(object):
         return self.buscaProf(arr, start, 0)
     
 
+    # Solução recursiva
     def buscaProf(self, arr, start, contador):
+        # Caso start menor que 0 ou satart maior que o tamanho do array
+        # Retorna False
         if start < 0 or start >= len(arr):
             return False
+        
+        # Caso o contador (inicialmente == 0) maior ou igual ao tamanho do array
+        # Retorna False
         if contador >= len(arr):
             return False
+    
+        # Caso o arr[start] == 0
+        # Retorna True
         if arr[start] == 0:
             return True
 
+        # Retorna buscando pela direita ou pela esquerda caso nenhum dos casos acima seja verdadeiro
         return self.buscaProf(arr, start + arr[start], contador + 1) or \
                self.buscaProf(arr, start - arr[start], contador + 1)
     
