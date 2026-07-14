ceb458bc4a6ebef48ec46d3a8b7104e75caaf4b5
Maria Fernanda Melo <mfsmelo06@gmail.com>
2026-07-14 12:56:31 -0300

    Time: 251 ms (77.01%), Space: 16 MB (93.51%) - LeetHub

---
+++ 1871-jump-game-vii/1871-jump-game-vii.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def canReach(self, s, minJump, maxJump):
+        q = deque([0])
+        mais_longe = 0
+
+        while q:
+            i = q.popleft()
+            comeco = max(i + minJump, mais_longe + 1)
+
+            for j in range (comeco, min(i + maxJump + 1, len(s))):
+                if s[j] == "0":
+                    q.append(j)
+                    if j == len(s) - 1:
+                        return True
+            mais_longe = i + maxJump
+        return False
