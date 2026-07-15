fbad747ccccb296c887b0db79f99ef1ef827c579
Maria Fernanda Melo <mfsmelo06@gmail.com>
2026-07-15 13:49:40 -0300

    Time: 115 ms (96.11%), Space: 31.2 MB (92.52%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.py
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -1,21 +1,27 @@
 class Solution:
     def maxResult(self, nums: List[int], k: int) -> int:
-        q = deque() # Cria um deque (fila duplamente encadeada) para guardar os índices estrategicamente
-        n = len(nums) # Captura o tamanho total do array
-        dp = [0] * n # Cria um array 'dp' preenchido com zeros, com o mesmo tamanho de 'nums', para guardar as somas máximas
+        q = deque() 
+        n = len(nums)
+        dp = [0] * n 
         
-        dp[n-1] = nums[n-1] # Define o caso base: se você já está na última casa, a pontuação é o próprio valor da casa
-        q.append(n-1) # Adiciona o último índice à fila
+        dp[n-1] = nums[n-1] 
+        q.append(n-1)
         
-        for i in range(n-2, -1, -1): # Percorre o array de trás para frente (começa do penúltimo e vai diminuindo até 0)
-            if q[0] - i > k: # Se a janela entre a posição atual 'i' e o melhor destino 'q[0]' ficou maior que o pulo permitido 'k'
-                q.popleft() # Remove o índice da frente (q[0]) da fila, pois ele ficou inalcançável a partir de 'i'
-                
-            dp[i] = nums[i] + dp[q[0]] # A pontuação na casa 'i' é o valor dessa casa + a melhor pontuação possível disponível na frente da fila
+        # Estratégia que olha de trás para frente
+        for i in range(n-2, -1, -1): 
+            # Se o pulo for maior que o limite(k) tira o indice de 'q'
+            if q[0] - i > k: 
+                q.popleft() 
             
-            while q and dp[q[-1]] < dp[i]: # Enquanto a fila tiver elementos E a pontuação do último elemento da fila for pior que a nova pontuação 'dp[i]'
-                q.pop() # Remove os elementos "fracos" do final da fila para manter a fila sempre em ordem decrescente de pontuação
-                
-            q.append(i) # Insere o índice atual na fila, já que ele agora é um "degrau" possível para os saltos das próximas iterações
+            # dp[i] recebe o somatório do maior valor da fila com o valor atual de nums[i]            
+            dp[i] = nums[i] + dp[q[0]]
             
-        return dp[0] # Ao terminar de percorrer todo o array até o início, o resultado máximo estará no índice 0
+            # Enquanto a fila não estiver vazia e o menor valor analisado for menor que o valor atual somado
+            while q and dp[q[-1]] < dp[i]: 
+                # retira o primeiro elemento da fila
+                q.pop() 
+
+            # adiciona à fila o índice atual    
+            q.append(i) 
+            
+        return dp[0] 
