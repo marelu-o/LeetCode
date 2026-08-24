014a2d21a20a3ed50bcab16cba6ff9de7e80804c
Maria Fernanda Melo <mfsmelo06@gmail.com>
2026-08-24 16:53:44 -0300

    Time: 0 ms (100%), Space: 30.4 MB (38.66%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -3,13 +3,38 @@ public:
     vector<int> sortedSquares(vector<int>& nums) {
 
         // Versão NÃO Otimizada
-        for(int i = 0; i < nums.size(); i++){
+        /*for(int i = 0; i < nums.size(); i++){
+            nums[i] = pow(nums[i], 2);
+        }
+        sort(nums.begin(), nums.end()); //O(n log n)
+        
+        return nums;*/ 
+
+        // Versão UM pouco mais Otimizada
+        /* for(int i = 0; i < nums.size(); i++){
             nums[i] = nums[i] * nums[i];
         }
         sort(nums.begin(), nums.end()); //O(n log n)
         
-        return nums;
+        return nums;*/ 
 
+
+        // Versão Otimizada usando ponteiros (O(n))
+        int esquerda = 0;
+        int direita = nums.size() - 1;
+        vector<int> resultado(nums.size());
+
+        for(int i = nums.size() - 1; i >= 0; i--){
+            if(abs(nums[esquerda] * nums[esquerda]) > abs(nums[direita] * nums[direita])){
+                resultado[i] = abs(nums[esquerda] * nums[esquerda]);
+                esquerda++;
+            } else {
+                resultado[i] = abs(nums[direita] * nums[direita]);
+                direita--;
+            }
+        }
+
+        return resultado;
         
     }
 };
