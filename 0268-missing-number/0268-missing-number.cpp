7a05e8c54756585ff3df1eeb5565d8ceb42a530c
Maria Fernanda Melo <mfsmelo06@gmail.com>
2026-08-24 15:59:28 -0300

    Time: 4 ms (18.22%), Space: 21.9 MB (38.74%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,7 +3,7 @@ public:
     int missingNumber(vector<int>& nums) {
 
         // Versão NÃO Otimizada
-        int soma_esperada = 0;
+       /* int soma_esperada = 0;
         for (int i = 0; i <= nums.size(); i++){
             soma_esperada += i;
         }
@@ -13,6 +13,17 @@ public:
             soma_real += nums[i];
         }
 
-        return soma_esperada - soma_real;
+        return soma_esperada - soma_real;*/
+
+        //Versão NÃO Otimizada com Sort
+        sort(nums.begin(), nums.end());
+
+        for (int j = 0; j < nums.size(); j++){
+            if(j != nums[j]){
+                return j;
+            }
+        }
+       return nums.size();
+
     }
 };
