ddfed0c6f923ff13061d7bacb9b763ed9e262790
Maria Fernanda Melo <mfsmelo06@gmail.com>
2026-08-24 15:30:56 -0300

    Time: 0 ms (100%), Space: 12.6 MB (12.97%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -2,11 +2,25 @@ class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
         vector<int> resultado;
-        int soma = 0;
+        //Versão NÃO Otimizada
+        // Para cada elemento no vetor original...
+        for (int i = 0; i < nums.size(); i++) {
+            int soma_atual = 0;
+            
+            // ...fazemos um NOVO laço voltando do início (0) até a posição atual (i)
+            for (int j = 0; j <= i; j++) { 
+                soma_atual += nums[j];
+            }
+            
+            resultado.push_back(soma_atual);
+        }
+
+        // Versão Otimizada
+       /* int soma = 0;
         for (int i = 0; i < nums.size(); i++){
             soma += nums[i];
             resultado.push_back(soma);
-        }
+        }*/
 
         return resultado;
 
