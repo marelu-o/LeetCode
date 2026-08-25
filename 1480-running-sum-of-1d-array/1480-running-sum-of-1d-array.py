5dc750500543745e725aa4c1680576975a89be72
Maria Fernanda Melo <mfsmelo06@gmail.com>
2026-08-25 15:08:51 -0300

    Time: 0 ms (100%), Space: 19.2 MB (79.63%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -4,19 +4,18 @@ class Solution:
         
         # Versão NÃO Otimizada O(n^2)
         # Para cada elemento no vetor original...
-
+        '''
         for i in range(len(nums)):
             soma_atual = 0
             # ...fazemos um NOVO laço voltando do início (0) até a posição atual (i)
             for j in range(i + 1):
                 soma_atual += nums[j]
             resultado.append(soma_atual)
-
-        # Versão Otimizada O(n)
         '''
+        # Versão Otimizada O(n)
         soma = 0
         for num in nums:
             soma += num
             resultado.append(soma)
-        '''
+            
         return resultado
