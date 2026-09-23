e3879b25c36c3f2a196dbac1bfe44eefc8fcde4c
Maria Fernanda Melo <mfsmelo06@gmail.com>
2026-09-22 23:52:49 -0300

    Time: 5 ms (33.74%), Space: 23.2 MB (99.45%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -1,6 +1,6 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-
+        '''
         # Uso de dicionário, onde a chave inicia com o valor igual à zero.
         caixa = {5: 0, 10: 0}
 
@@ -27,6 +27,38 @@ class Solution:
                     caixa[5] -= 1
                 elif caixa[5] >= 3:
                     caixa[5] -= 3
+                else:
+                    return False
+
+        return True   
+        '''
+
+        cinco = 0
+        dez = 0
+
+        # Percorre as notas presentes no vetor bills
+        for nota in bills:
+
+            # Verifica se a nota recebida já está em nosso caixa
+            # Caso nota == 5, acrescentamos mais uma nota ao caixa
+            if nota == 5:
+                cinco += 1
+
+            # Caso nota == 10, acrescentamos mais uma nota no caixa e retiramos uma nota de 5
+            elif nota == 10:
+                if cinco > 0:
+                    cinco -= 1
+                    dez += 1
+                else:
+                    return False
+
+            # Caso nota == 20, acrescentamos mais uma nota ao nosso caixa e retiramos um nota de 10 e outra de 5 OU retiramos três notas de 5
+            elif nota == 20:
+                if dez > 0 and cinco > 0:
+                    dez -= 1
+                    cinco -= 1
+                elif cinco >= 3:
+                    cinco -= 3
                 else:
                     return False
 
